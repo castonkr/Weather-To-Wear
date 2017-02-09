c24cc5676e4a2e58488fe86afb2cfeb9858dabbe
Kiana Caston <castonkr@rose-hulman.edu>
2017-02-09 18:20:15 -0500

    ClothingItems
    HardCode ClothingItemsList list

--- app/src/main/java/edu/rosehulman/schaffll/weathertowear/ClothingItemList.java
+++ app/src/main/java/edu/rosehulman/schaffll/weathertowear/ClothingItemList.java
@@ -9,17 +9,70 @@ import java.util.List;
 
 public class ClothingItemList {
 
-    public List clothingItemList = new ArrayList<>();
-
+    public List allClothingItemList = new ArrayList<>();
+    private List mClothingItemList = new ArrayList<>();
 
     {
-        ClothingItem blouse = new ClothingItem("blouse", 0, 0, 0);
-        ClothingItem boots = new ClothingItem("boots", 6, -2, 1);
+        ClothingItem blouse = new ClothingItem("Blouse", 0, 0, 0);
+        ClothingItem boots = new ClothingItem("Boots", 6, -2, 1);
+        ClothingItem cardigan = new ClothingItem("Cardigan", 0, 0, 0);
+        ClothingItem dress = new ClothingItem("Dress", 0, 0, 0);
+        ClothingItem gloves = new ClothingItem("Gloves", 0, 0, 0);
+        ClothingItem hat = new ClothingItem("Hat", 0, 0, 0);
+        ClothingItem hoody = new ClothingItem("Hoody", 0, 0, 0);
+        ClothingItem jacket = new ClothingItem("Jacket", 0, 0, 0);
+        ClothingItem jeans = new ClothingItem("Jeans", 0, 0, 0);
+        ClothingItem sandals = new ClothingItem("Sandals", 0, 0, 0);
+        ClothingItem scarf = new ClothingItem("Scarf", 0, 0, 0);
+        ClothingItem shirt = new ClothingItem("Shirt", 0, 0, 0);
+        ClothingItem skirt = new ClothingItem("Skirt", 0, 0, 0);
+        ClothingItem sneakers = new ClothingItem("Sneakers", 0, 0, 0);
+        ClothingItem sweater = new ClothingItem("Sweater", 0, 0, 0);
+        ClothingItem sweatpants = new ClothingItem("Sweatpants", 0, 0, 0);
+        ClothingItem sweatshirt = new ClothingItem("Sweatshirt", 0, 0, 0);
+        ClothingItem tshirt = new ClothingItem("T-Shirt", 0, 0, 0);
+        ClothingItem tanktop = new ClothingItem("Tanktop", 0, 0, 0);
+        ClothingItem umbrella = new ClothingItem("Umbrella", 0, 0, 0);
+        ClothingItem wintercoat = new ClothingItem("Winter Coat", 0, 0, 0);
 
 
-        clothingItemList.add(blouse);
-        clothingItemList.add(boots);
+        allClothingItemList.add(blouse);
+        allClothingItemList.add(boots);
+        allClothingItemList.add(cardigan);
+        allClothingItemList.add(dress);
+        allClothingItemList.add(gloves);
+        allClothingItemList.add(hat);
+        allClothingItemList.add(hoody);
+        allClothingItemList.add(jacket);
+        allClothingItemList.add(jeans);
+        allClothingItemList.add(sandals);
+        allClothingItemList.add(scarf);
+        allClothingItemList.add(shirt);
+        allClothingItemList.add(skirt);
+        allClothingItemList.add(sneakers);
+        allClothingItemList.add(sweater);
+        allClothingItemList.add(sweatpants);
+        allClothingItemList.add(sweatshirt);
+        allClothingItemList.add(tshirt);
+        allClothingItemList.add(tanktop);
+        allClothingItemList.add(umbrella);
+        allClothingItemList.add(wintercoat);
+
     }
+
+    public ClothingItemList() {
+
+    }
+
+    public void add(int pos) {
+        mClothingItemList.add(allClothingItemList.get(pos));
+    }
+
+    public void remove(int pos) {
+        mClothingItemList.remove(allClothingItemList.get(pos));
+    }
+
+
 
 
 }
