4f60713af94848b66be0cd90af28b78536e2a6c0
Kiana Caston <castonkr@rose-hulman.edu>
2017-02-12 16:05:58 -0500

    Fixed Merge?

--- app/src/main/java/edu/rosehulman/schaffll/weathertowear/Weather/Calculations.java
+++ app/src/main/java/edu/rosehulman/schaffll/weathertowear/Weather/Calculations.java
@@ -44,15 +44,7 @@ public class Calculations {
         mColdTemp = coldTemp;
         mCurrentTemp = currentTemp;
         mWeatherID = weatherID;
-<<<<<<< HEAD
         mBoolList = boolList;
-
-
-=======
-        mUserRef = userRef;
-        cil = new ClothingItemList();
-//        mBoolList = new boolean[21];
->>>>>>> origin/master
 
         int medianTemp = (coldTemp + hotTemp) / 2;
 
@@ -76,110 +68,13 @@ public class Calculations {
             weatherNum = 2;
         }
 
-<<<<<<< HEAD
         ClothingItemList mClothingItems = new ClothingItemList(mBoolList);
         userClothingItems = mClothingItems.getReturnClothingItemList();
-=======
-        //have weather num
 
-        //this will get us to the reference https://weather-to-wear.firebaseio.com/users/QFFVDeWJR4Xs84FpdetUCk1dovG3/booleanArray
-        //from there, we need to go through all of the clothing items and see if they are true and false
-        //put those into a boolean array
-        //
-
-       DatabaseReference booleanArray = mUserRef.child("booleanArray");
-        Log.d("firebare", ""+booleanArray);
-        //booleanArray.orderByChild("booleanArray").equalTo("true");
-        //String k = booleanArray.child("0").getKey();
-       // booleanArray.
-        //Query booleanArray = mUserRef.orderByChild("booleanArray").equalTo("true");
-        //Query trueClothing = booleanArray.orderByKey().equalTo("true");
-        booleanArray.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                int position = Integer.parseInt(dataSnapshot.getKey());
-                mBoolList[position] = (boolean) dataSnapshot.getValue();
-                boolean temp = (boolean) dataSnapshot.getValue();
-                cil.addBoolean(temp, position);
-                //        boolean b = mBoolList[0];
-                //setBooleanList(temp, position);
-                if (temp != false){
-                    mBoolList[position] = true;
-                    Log.d("help3", "not false" + position + "");
-                    setBooleanList(true, position);
-                }
-                else{
-                    setBooleanList(false, position);
-                }
-                Log.d("helppppp", "has occured");
-                               // temp = mBoolList[position];
-//                if(temp){
-//                    String k = "true";
-//                    Log.d("help3", k);
-//                }
-
-//        String k;
-//        if (temp){
-//            k = "true";
-//        }
-//        if (temp == false){
-//            k = "faslse";
-//        }
-//        else{
-//            k = "null";
-//        }
-               // Log.d("test2", k);
-//                if (mBoolList[position]) {
-//                    //mAdapter.addItem(mClothingItems[position]);
-//                    //mClothingItemList.add(position);
-//
-//                }
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-//        boolean b = mBoolList[0];
-//        String k;
-//        if (b == true){
-//            k = "true";
-//        }
-//        if (b == false){
-//            k = "faslse";
-//        }
-//        else{
-//            k = "null";
-//        }
-
-       // Log.d("test", k);
-        boolean[] callBooleanList = mBoolList;
         userClothingItems = getReturnClothingItemList();
         //getReturnClothingItemList();
         int i = userClothingItems.size();
-        Log.d("tst", "idk: "+ i);
-        //cil.getReturnClothingItemList();
-//        ClothingItemList mClothingItems = new ClothingItemList(mBoolList);
-//        userClothingItems = mClothingItems.getReturnClothingItemList();
-//        createNewOutfits();
->>>>>>> origin/master
+
 
 
 
